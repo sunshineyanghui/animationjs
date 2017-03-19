4b9de4f85782437c187c18646c9503f80f04ffdc
yanghui <1187073986@qq.com>
2017-03-19 13:53:30 +0800

    one animation

---
+++ demo/index.js
@@ -0,0 +1,22 @@
+var imgUrl = 'rabbit.png';
+var positions = ["0 -854", "-174 -852", "-349 -852", "-524 -852", "-698 -851", "-873 -848"];
+var ele = document.getElementById('rabbit');
+animation( ele,positions,imgUrl);
+function animation (ele,positions,imgUrl){
+	ele.style.backgroundImage = 'url(' + imgUrl + ')';
+	ele.style.backgroundRepeat ='no-repeat';
+
+	var index = 0;
+	function run (){
+		var position = positions[index].split(' ');
+		ele.style.backgroundPosition = position[0] + 'px ' + position[1] + 'px';
+		index++;
+		if (index >= positions.length) {
+			index = 0;
+		}
+		setTimeout(run, 80);
+	}
+
+	run();
+
+}
